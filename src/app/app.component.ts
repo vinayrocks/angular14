7f3495412f830c65e49c0baefcc458e78c764c3a
vinay <vinayrocks12691@gmail.com>
2022-11-27 08:12:08 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component, Renderer2, Inject, ElementRef, ViewChild, OnInit, AfterViewInit } from '@angular/core';
-import { BehaviorSubject, Subscription } from 'rxjs';
-import { RdNavbarComponent } from './core/components/rd-navbar/rd-navbar.component';
+import { Component, Inject, OnInit, AfterViewInit } from '@angular/core';
+import {Subscription } from 'rxjs';
 import { Router, NavigationEnd } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
-import { Location } from '@angular/common';
+import { DOCUMENT,Location } from '@angular/common';
 import { RdAuthenticateService } from './shared/services/authentication/rd-authenticate.service';
 import { NotificationService } from './shared/services/common/rd-notification/notification.service';
 import { filter } from 'rxjs/operators';
@@ -14,13 +12,13 @@ import { filter } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit, AfterViewInit {
     title = 'radian';
-    private _router: Subscription;
     currentUser: any=[];
     loading: any;
+    _showHeader:boolean=false;
     // @ViewChild(RdNavbarComponent) navbar: RdNavbarComponent;
 
     constructor(private location: Location, private rdAuthenticateService: RdAuthenticateService,
-        private router: Router, @Inject(DOCUMENT,) private notificationService: NotificationService) {
+        private router: Router) {
 
             this.router.events.pipe(
                 filter(event => event instanceof NavigationEnd)
@@ -34,34 +32,22 @@ export class AppComponent implements OnInit, AfterViewInit {
                     window.document.activeElement.scrollTop = 0;
                 }
                 var _location = this.location.path();
-                _location = _location.split('/')[1];
-                if (this.currentUser !== null && _location !== 'our-purpose' && _location !== 'vission-mission'
-                    && _location !== 'member-detail' && _location !== 'detail' && _location !== 'portfolio-detail'
-                    && _location !== 'our-process' && _location !== 'updates' && _location !== 'search' && _location !== 'resetpassword'
-                ) {
-                    //this.router.navigate(['/member/hunar_view']);
+                // _location = _location.split('/')[1];
+                console.log(_location.split('/')[2])
+                if(_location.split('/')[2] !== 'login'){
+                    this._showHeader = true;
+                } else {
+                    this._showHeader = false; 
                 }
-            });
-        // this._router = this.router.events.filter(event => event instanceof NavigationEnd)
-        //     .subscribe((event: NavigationEnd) => {
-        //         // this.loading = this.notificationService.loading;
-        //         // console.log(this.loading);
-        //         this.currentUser = this.rdAuthenticateService.getLocalStorageData();
-        //         if (window.outerWidth > 991) {
-        //             window.document.children[0].scrollTop = 0;
-        //         } else {
-        //             window.document.activeElement.scrollTop = 0;
-        //         }
-        //         var _location = this.location.path();
-        //         _location = _location.split('/')[1];
-        //         if (this.currentUser !== null && _location !== 'our-purpose' && _location !== 'vission-mission'
-        //             && _location !== 'member-detail' && _location !== 'detail' && _location !== 'portfolio-detail'
-        //             && _location !== 'our-process' && _location !== 'updates' && _location !== 'search' && _location !== 'resetpassword'
-        //         ) {
-        //             //this.router.navigate(['/member/hunar_view']);
-        //         }
 
-        //     });
+
+                // if (this.currentUser !== null && _location !== 'our-purpose' && _location !== 'vission-mission'
+                //     && _location !== 'member-detail' && _location !== 'detail' && _location !== 'portfolio-detail'
+                //     && _location !== 'our-process' && _location !== 'updates' && _location !== 'search' && _location !== 'resetpassword'
+                // ) {
+                //     //this.router.navigate(['/member/hunar_view']);
+                // }
+            });
     }
     ngOnInit() {
 
