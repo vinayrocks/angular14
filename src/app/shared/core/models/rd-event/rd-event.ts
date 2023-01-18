3bce461baf64c03712075bd058851743dc90a575
vinay <vinayrocks12691@gmail.com>
2023-01-18 23:41:30 +0530

    changes

--- src/app/shared/core/models/rd-event/rd-event.ts
+++ src/app/shared/core/models/rd-event/rd-event.ts
@@ -1,26 +1,26 @@
 export class RdEvent {
-  Id:Number=0;
-  EventName: String='';
-  EventDescription:String='';
-  EventMedia: String='';
-  EventSkill:String='';
-  EventCategory:String='';
-  IsEventOnline:String='';
-  EventLink:String='';
-  country: string;
-  street: string;
-  city: string;
-  state: string;
-  zip: string;
-  EventStartDateTime: string;
-  EventEndDateTime: string;
-  UserId:Number=0;
-  Email:String='';
-  FirstName:String='';
-  EventStatus:Number=1;
-  EventStatusDate:Date=new Date();
+  Id: Number = 0;
+  EventName: String = "";
+  EventDescription: String = "";
+  EventMedia: String = "";
+  EventSkill: String = "";
+  EventCategory: String = "";
+  IsEventOnline: String = "";
+  EventLink: String = "";
+  country: string = "";
+  street: string = "";
+  city: string = "";
+  state: string = "";
+  zip: string = "";
+  EventStartDateTime: string = "";
+  EventEndDateTime: string = "";
+  UserId: Number = 0;
+  Email: String = "";
+  FirstName: String = "";
+  EventStatus: Number = 1;
+  EventStatusDate: Date = new Date();
   public rdRadian: RdEvent[];
-  constructor(input: any){
+  constructor(input: any) {
     Object.assign(this, input);
     return this;
   }
